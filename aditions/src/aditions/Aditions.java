e7ba0604e0d6e4713024713c097086bfeb1858cc
Sobhakpd <sobha104dev@gmail.com>
2023-05-31 15:04:22 +0530

    this is adition commit

---
+++ aditions/src/aditions/Aditions.java
@@ -0,0 +1,16 @@
+package aditions;
+
+public class Aditions {
+
+	public static void main(String[] args) {
+		int num1 =10;
+		int num2 = 20;
+		int num3 = 30;
+		int result;
+		result =num1+num2+num3;
+		System.out.println("Addition of two number is :" +result);
+
+	}
+
+
+}
